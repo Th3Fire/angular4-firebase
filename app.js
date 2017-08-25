09ac9717a5de6c4c1dd1b9237e720731ee562e36
Wuttinan Chaoyos <wuttinan.cha@gec.co.th>
2017-08-25 14:38:57 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const app = express();
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-app.use(forceSSL());
+ let express = require("express");
+  var path = require('path');
+  let app = require('express')();
+  let http = require('http').Server(app);
 
-const path = require('path');
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-// Heroku port
-app.listen(process.env.PORT || 8080);
+   app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+  app.get('*', function(req, res) {
+   res.sendFile(path.join(__dirname, 'dist/index.html'));
+   });
+
+
+
+
+   const port = process.env.PORT || '80';
+   app.set('port', port);
+
+   http.listen(port, function() {
+   console.log('listening on *:80');
+ });
