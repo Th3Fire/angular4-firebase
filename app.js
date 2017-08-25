ec6ed12716f0a4bc8de02523a8029785db71bdc8
Wuttinan Chaoyos <wuttinan.cha@gec.co.th>
2017-08-25 13:55:55 +0700

    initial deploy

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const app = express();
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -14,15 +10,10 @@ const forceSSL = function() {
     next();
   }
 }
-// Instruct the app
-// to use the forceSSL
-// middleware
 app.use(forceSSL());
 
 const path = require('path');
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
