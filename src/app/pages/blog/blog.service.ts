4f1164787c39f8346a262ecb6c4edca70c5f0e27
Wuttinan Chaoyos <wuttinan.cha@gec.co.th>
2017-08-30 18:24:05 +0700

    add blog

---
+++ src/app/pages/blog/blog.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class BlogService {
+  constructor(public af: AngularFireDatabase) { }
+
+  getAll(limit=30) {
+    return this.af.list('blog', {
+      query: {
+        limitToLast: limit,
+      }
+    });
+    }
+}
