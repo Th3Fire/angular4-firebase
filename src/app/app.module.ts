65af9b9068f0acd57abda4cfd7c6d0e4f6deb3ef
Wuttinan Chaoyos <wuttinan.cha@gec.co.th>
2017-08-31 16:27:02 +0700

    test add full screen back drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,6 +70,7 @@ import { TruncatePipe } from './pages/blog/truncate.pipe';
     LoadingModule.forRoot({
         animationType: ANIMATION_TYPES.wanderingCubes,
         backdropBackgroundColour: 'rgba(0,0,0,0.1)', 
+        fullScreenBackdrop: true,
         backdropBorderRadius: '4px',
         primaryColour: '#ffffff', 
         secondaryColour: '#ffffff', 
